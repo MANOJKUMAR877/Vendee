513123351f81d11ca31da8b311485a3eb7495038
manojkumar <manoj.kumar@innoventes.co>
2020-07-19 23:58:54 +0530

    Redux added

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,10 @@
-import {createStore,combineReducers }from 'redux'
-const initialState={};
-const reducer =combineReducers({
-    productList:productListReducer,
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import {productListReducer} from './reducers/productReducer';
+import thunk from 'redux-thunk'
+const initialState = {};
+const reducer = combineReducers({
+    productList: productListReducer,
 })
-const store =createStore(reducer,initialState)
+const composeEnchancer=window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnchancer(applyMiddleware(thunk)))
+export default store;
